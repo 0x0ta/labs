ace35e8364b453fed641d7ab23eb3a9d4ad9017a
ota__ <hayley.samantha86@gmail.com>
2021-10-06 17:53:15 +0100

    Update fake_binary.py

--- codewars/PythonKatas/fake_binary.py
+++ codewars/PythonKatas/fake_binary.py
@@ -1,8 +1,2 @@
 def fake_bin(x):
-    total = ""
-    for each in x:
-        if int(each) < 5:
-            total = total + "0"
-        else:
-            total = total + "1"
-    return total
+    return "".join("0" if int(each) < 5 else "1" for each in x)
