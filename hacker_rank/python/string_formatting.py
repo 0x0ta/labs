44bf34e719176326201d04ef72fee1f7c0e41da6
0x0ta <hayley.samantha86@gmail.com>
2022-04-14 22:53:54 +0100

    Added solution for string_formatting challenge

---
+++ hacker_rank/python/string_formatting.py
@@ -0,0 +1,13 @@
+number = 17
+
+def print_formatted(number):
+    spacer = len(str(format(number, 'b'))) +1#.replace('0b',''))) + 1
+    for i in range(1, int(number+1)):
+        a = i
+        b = format(i, 'o')#.replace('0o', '')
+        c = format(i, 'X')
+        d = format(i, 'b')
+        
+        print(f"{' ' * (spacer-1-len(str(a)))}{a}{' ' * (spacer-len(str(b)))}{b}{' ' * (spacer-len(str(c)))}{c}{' ' * (spacer-len(str(d)))}{d}")
+
+print_formatted(number)    
