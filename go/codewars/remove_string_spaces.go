5dd2144a8f7c963bacde0c681c8bac7b872b66b4
0x0ta <hayley.samantha86@gmail.com>
2022-05-23 02:28:06 +0100

    Added Go solution for Remove String Spaces challenge

---
+++ go/codewars/remove_string_spaces.go
@@ -0,0 +1,15 @@
+package kata
+
+import (
+  "strings"
+)
+
+func NoSpace(word string) string {
+  out := ""
+  v := strings.Split(word, " ")
+  for i := 0; i < len(v); i++ {
+    out = out + v[i]
+  }
+  return out
+  
+}
