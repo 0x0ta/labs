945d4bdc5ab0a24d30e9e966051631bc7202ef55
0x0ta <hayley.samantha86@gmail.com>
2022-06-04 23:37:30 +0100

    added a solution for Abbreviate a two word name callenge

---
+++ go/codewars/abbreviate_a_two_word_name.go
@@ -0,0 +1,11 @@
+package kata
+
+import (
+	"strings"
+)
+
+func AbbrevName(name string) string {
+	names := strings.Split(name, " ")
+	abbr := string(names[0][0]) + "." + string(names[1][0])
+	return abbr
+}
