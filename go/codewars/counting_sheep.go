1fefe11c5be34fbd55c775d777b6cc288f5b3bb3
0x0ta <hayley.samantha86@gmail.com>
2022-06-04 13:34:40 +0100

    added solution for counting sheep challenge

---
+++ go/codewars/counting_sheep.go
@@ -0,0 +1,18 @@
+package kata
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func countSheep(num int) string {
+	output := ""
+	sheep := " sheep..."
+	for i := 0; i < num; i++ {
+		o := fmt.Sprintf(strconv.Itoa(i+1) + sheep)
+		output += o
+	}
+
+	return output
+
+}
