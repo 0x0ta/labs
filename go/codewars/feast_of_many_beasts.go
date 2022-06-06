dba4adde56d13f48f79f6b427b681d46c8d9cfe1
0x0ta <hayley.samantha86@gmail.com>
2022-06-06 23:34:30 +0100

    refactored go solution for feast of many beasts

--- go/codewars/feast_of_many_beasts.go
+++ go/codewars/feast_of_many_beasts.go
@@ -1,9 +1,5 @@
 package kata
 
 func Feast(beast string, dish string) bool {
-	if beast[0] == dish[0] && beast[len(beast)-1] == dish[len(dish)-1] {
-		return true
-	} else {
-		return false
-	}
+	return beast[0] == dish[0] && beast[len(beast)-1] == dish[len(dish)-1]
 }
